aa5e0426050c55a0b3012e575c1de13da03c320d
Lyxnn10 <93597031+Lyxnn10@users.noreply.github.com>
2023-02-20 23:06:22 +0100

    Update ws2812b.py

--- ws2812b.py
+++ ws2812b.py
@@ -16,9 +16,7 @@ def ws2812():
     nop()                   .side(0)    [T2 - 1]
     wrap()
     
-#delay here is the reset time. You need a pause to reset the LED strip back to the initial LED
-#however, if you have quite a bit of processing to do before the next time you update the strip
-#you could put in delay=0 (or a lower delay)
+
 class ws2812b:
     def __init__(self, num_leds, state_machine, pin, delay=0.001):
         self.pixels = array.array("I", [0 for _ in range(num_leds)])
@@ -28,7 +26,6 @@ class ws2812b:
         self.delay = delay
         self.brightnessvalue = 255
 
-    # Set the overal value to adjust brightness when updating leds
     def brightness(self, brightness = None):
         if brightness == None:
             return self.brightnessvalue
@@ -39,7 +36,6 @@ class ws2812b:
             brightness = 255
         self.brightnessvalue = brightness
 
-      # Create a gradient with two RGB colors between "pixel1" and "pixel2" (inclusive)
     def set_pixel_line_gradient(self, pixel1, pixel2, left_red, left_green, left_blue, right_red, right_green, right_blue):
         if pixel2 - pixel1 == 0: return
     
@@ -54,26 +50,22 @@ class ws2812b:
             
             self.set_pixel(left_pixel + i, red, green, blue)
     
-      # Set an array of pixels starting from "pixel1" to "pixel2" to the desired color.
     def set_pixel_line(self, pixel1, pixel2, red, green, blue):
         for i in range(pixel1, pixel2+1):
             self.set_pixel(i, red, green, blue)
 
     def set_pixel(self, pixel_num, red, green, blue):
-        # Adjust color values with brightnesslevel
         blue = round(blue * (self.brightness() / 255))
         red = round(red * (self.brightness() / 255))
         green = round(green * (self.brightness() / 255))
 
         self.pixels[pixel_num] = blue | red << 8 | green << 16
     
-    # rotate x pixels to the left
     def rotate_left(self, num_of_pixels):
         if num_of_pixels == None:
             num_of_pixels = 1
         self.pixels = self.pixels[num_of_pixels:] + self.pixels[:num_of_pixels]
 
-    # rotate x pixels to the right
     def rotate_right(self, num_of_pixels):
         if num_of_pixels == None:
             num_of_pixels = 1
